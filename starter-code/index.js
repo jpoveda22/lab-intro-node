c21765475b49c3a5d9e25a8a8fd64d5fa691dd1d
jpoveda22 <jpoveda226@gmail.com>
2020-03-03 10:56:07 -0500

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,66 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length=0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a-b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos]){
+            throw new Error('OutOfBounds')
+          }
+          return this.items[pos]
+        }
+  
 
-  max() {}
+  max()  {
+        if(this.items.length ===0){
+          throw new Error('EmptySortedList')
+        }
+        return this.items[this.items.length-1]
+      }
 
-  min() {}
+  min() {
+    if(this.items.length ===0){
+      throw new Error('EmptySortedList')
+    }
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    let sum = this.items.reduce(function(acc, curr){
+      return acc + curr
+    }, 0)
+    return sum
+  }
 
-  avg() {}
-}
+  avg() {
+        if(this.items.length ===0){
+          throw new Error('EmptySortedList')
+        }
+        let sum = this.items.reduce(function(acc, curr){
+          return acc + curr
+        }, 0)
+        return sum/this.items.length
+      }
+    }
 
 module.exports = SortedList;
+
+
+
+
+
+
+
+
+
+
+
+
+
